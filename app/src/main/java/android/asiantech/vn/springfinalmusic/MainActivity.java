c70dde2c8c63bd4f7e68b057456dece61d260dbe
Tai Nguyen D <tai.nguyen@asiantech.vn>
2018-05-18 18:03:11 +0700

    test CI

--- app/src/main/java/android/asiantech/vn/springfinalmusic/MainActivity.java
+++ app/src/main/java/android/asiantech/vn/springfinalmusic/MainActivity.java
@@ -2,12 +2,17 @@ package android.asiantech.vn.springfinalmusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView TEXT_VIEW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TEXT_VIEW  = findViewById(R.id.tvContent);
+        TEXT_VIEW.setText("test CI");
     }
 }
