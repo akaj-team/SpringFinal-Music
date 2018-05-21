b17f94ff41597f5efb440cb8468ae71c0db16ef5
hien.dao <hien.dao@asiantech.vn>
2018-05-21 13:40:29 +0700

    add minibar home

---
+++ app/src/main/java/android/asiantech/vn/springfinalmusic/home/HomeActivity.java
@@ -0,0 +1,14 @@
+package android.asiantech.vn.springfinalmusic.home;
+
+import android.app.Activity;
+import android.asiantech.vn.springfinalmusic.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
