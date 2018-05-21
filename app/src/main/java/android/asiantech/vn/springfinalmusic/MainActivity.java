191d380dfad6e6865460b2c0443c8338ff1750fe
Phuc Nguyen V <phuc.nguyen3@asiantech.vn>
2018-05-21 11:45:30 +0700

    App don gian de test

--- app/src/main/java/android/asiantech/vn/springfinalmusic/MainActivity.java
+++ app/src/main/java/android/asiantech/vn/springfinalmusic/MainActivity.java
@@ -2,12 +2,22 @@ package android.asiantech.vn.springfinalmusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest = findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Do some thing
+            }
+        });
     }
 }
