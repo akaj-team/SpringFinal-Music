c717474e78f6f57e403c0c34ddd24ebb0a1e8154
hien.dao <hien.dao@asiantech.vn>
2018-05-21 09:30:23 +0700

    create layoyt loading

---
+++ app/src/main/java/Loading/LoadingActivity.java
@@ -0,0 +1,14 @@
+package Loading;
+
+import android.app.Activity;
+import android.asiantech.vn.springfinalmusic.R;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
